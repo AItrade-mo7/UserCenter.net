fdd17e5e84a8bfb602b9e8dfaf850dc9c7a1b0e3
墨七 <meichangliang@mo7.cc>
2022-07-17 02:52:34 +0800

    exit-push

--- src/pages/CoinServe/lib/InfoOk.vue
+++ src/pages/CoinServe/lib/InfoOk.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { NewSocket } from '@/api/CoinAI/CoinAIWss';
 import { onMounted, onUnmounted } from 'vue';
+import type { WssDataType } from '@/api/CoinAI/WssType.d';
+import type { resDataType } from '@/utils/utils.d';
+
 const props = defineProps({
   config: Object,
 });
 
-let wssData = $ref({});
+let WssData: WssDataType = $ref({});
+let ResData: resDataType = $ref({});
 let socketObj: any;
 
 onMounted(() => {
@@ -13,8 +17,9 @@ onMounted(() => {
     socketObj = NewSocket({
       Host: props.config.AppEnv.CoinServeID,
       MessageEvent(res) {
+        ResData = res.Response;
         if (res.Response.Code == 1) {
-          wssData = res.Response.Data;
+          WssData = res.Response.Data;
         }
       },
     });
@@ -27,10 +32,55 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="InfoOk" v-if="wssData.DataSource">
-    {{ wssData }}
+  <div>{{ WssData }}</div>
+  <div class="InfoOk" v-if="WssData.DataSource">
+    <div class="title">系统状态</div>
+    <n-space class="data-wrapper">
+      <div class="block">
+        <span class="label">连接状态</span>
+        <span class="value green">{{ ResData.Msg }}</span>
+      </div>
+      <div class="block">
+        <span class="label">系统时间</span>
+        <span class="value red">{{ WssData.SysTime }}</span>
+      </div>
+    </n-space>
   </div>
-  <div v-else>正在连接系统。。。</div>
+  <div v-if="!WssData.DataSource">
+    正在连接到 <span class="lineHight">{{ props.config.AppEnv.CoinServeID }}</span> .....
+  </div>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+@import '@/config/constant.less';
+.lineHight {
+  color: @mainColor;
+}
+
+.title {
+  font-weight: 700;
+  font-size: 18px;
+}
+
+.green {
+  color: @greenColor;
+}
+.red {
+  color: @redColor;
+}
+
+.data-wrapper {
+  .block {
+    font-size: 14px;
+    .label {
+      color: #666;
+      font-size: 12px;
+      &::after {
+        content: ' : ';
+      }
+    }
+    .value {
+    }
+  }
+}
+</style>
