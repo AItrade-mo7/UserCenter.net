45e0147ea70c08371e915c046b1eb98c447cd81d
墨七 <meichangliang@mo7.cc>
2022-07-16 18:50:06 +0800

    exit-push

--- _shell/deploy.sh
+++ _shell/deploy.sh
@@ -3,12 +3,12 @@
 source "./_shell/init.sh"
 #############
 
-# npm install
-# echo "开始打包" &&
-#   npm run build
+npm install
+echo "开始打包" &&
+  npm run build
 
-# echo "停止 pm2 服务" &&
-#   pm2 delete ${startName}
+echo "停止 pm2 服务" &&
+  pm2 delete ${startName}
 
 echo "移动文件到 ProdProject 目录"
 cp -r ${outPutPath}"/." ${deployPath}"/"
