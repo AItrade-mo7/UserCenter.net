23fe89980a1d7b20fb914b5bd9c7216401aba94f
墨七 <meichangliang@mo7.cc>
2022-07-20 11:21:58 +0800

    exit-push

--- server/router/wss/WsServer.go
+++ server/router/wss/WsServer.go
@@ -3,6 +3,7 @@ package wss
 import (
 	"time"
 
+	"github.com/EasyGolang/goTools/mRes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 	jsoniter "github.com/json-iterator/go"
@@ -11,27 +12,24 @@ import (
 // webSocket请求ping 返回pong
 func WsServer() func(*fiber.Ctx) error {
 	return websocket.New(func(ws *websocket.Conn) {
-		AuthStatus := -1
+		AuthResult := mRes.ResType{}
 
 		var (
-			mt  int
 			msg []byte
 			err error
 		)
 
 		go func() {
 			for {
-				mt, msg, err = ws.ReadMessage()
+				_, msg, err = ws.ReadMessage()
 				if err != nil {
 					ws.Close()
 					break
 				}
+				if AuthResult.Code < 0 {
+					AuthResult = Auth(msg)
+				}
 
-				SendData := Read(msg)
-				AuthStatus = SendData.Code
-
-				b, _ := jsoniter.Marshal(SendData)
-				ws.WriteMessage(mt, b)
 				time.Sleep(time.Second) // 一秒执行一次
 			}
 		}()
@@ -39,7 +37,7 @@ func WsServer() func(*fiber.Ctx) error {
 		for {
 			SendData := Send()
 
-			if AuthStatus > 0 {
+			if AuthResult.Code > 0 {
 				b, _ := jsoniter.Marshal(SendData)
 
 				err := ws.WriteMessage(1, b)
@@ -47,6 +45,7 @@ func WsServer() func(*fiber.Ctx) error {
 					ws.Close()
 					break
 				}
+			} else {
 			}
 
 			time.Sleep(time.Second) // 一秒执行一次
