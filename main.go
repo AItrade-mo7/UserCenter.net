ba61ff4f1ce99a22727739f197999ea98fd4a706
墨七 <meichangliang@mo7.cc>
2022-07-28 01:42:45 +0800

    exit-push

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	_ "embed"
+	"time"
 
 	"DataCenter.net/server/global"
 	"DataCenter.net/server/global/config"
 	"DataCenter.net/server/router"
+	"DataCenter.net/server/tmpl"
 	jsoniter "github.com/json-iterator/go"
 )
 
@@ -16,6 +18,18 @@ func main() {
 	jsoniter.Unmarshal(AppPackage, &config.AppInfo)
 	// 初始化系统参数
 	global.Start()
+
+	go global.Email(global.EmailOpt{
+		To: []string{
+			"meichangliang@mo7.cc",
+		},
+		Subject:  "ServeStart",
+		Template: tmpl.SysEmail,
+		SendData: tmpl.SysParam{
+			Message: "服务启动",
+			SysTime: time.Now(),
+		},
+	}).Send()
 
 	// 启动 http 监听服务
 	router.Start()
