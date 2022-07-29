43d4ce1fe80ccb73ecff3afa631908e20305778c
墨七 <meichangliang@mo7.cc>
2022-07-30 07:44:34 +0800

    exit-push

--- src/pages/Market/lib/TickerAnalyse.vue
+++ src/pages/Market/lib/TickerAnalyse.vue
@@ -15,15 +15,62 @@ onMounted(() => {
   GetData();
 });
 
-const CountUR = (ur: string) => {
-  const Num = parseInt(ur, 10);
-  if (Num > 0) {
+const UPIndex = () => {
+  if (Data.UPIndex - 50 > 0) {
     return 'green';
   }
-  if (Num < 0) {
+  if (Data.UPIndex - 50 < 0) {
     return 'red';
   }
   return '';
+};
+
+const UDAvg = () => {
+  if (Data.UDAvg - 0 > 0) {
+    return 'green';
+  }
+  if (Data.UDAvg - 0 < 0) {
+    return 'red';
+  }
+  return '';
+};
+
+const DirIndex = () => {
+  const Return = {
+    style: '',
+    text: '震荡',
+    value: Data.DirIndex,
+  };
+
+  if (Data.DirIndex - 0 > 0) {
+    Return.style = 'green';
+    Return.text = '上涨';
+  }
+  if (Data.DirIndex - 0 < 0) {
+    Return.style = 'red';
+    Return.text = '下跌';
+  }
+
+  return Return;
+};
+
+const CoinTicker = (label: string) => {
+  const Coin = Data[label];
+  const Return = {
+    CcyName: Coin.CcyName,
+    U_R24: Coin.U_R24,
+    style: '',
+    VolIdx: Coin.VolIdx,
+  };
+
+  if (Return.U_R24 - 0 > 0) {
+    Return.style = 'green';
+  }
+  if (Return.U_R24 - 0 < 0) {
+    Return.style = 'red';
+  }
+
+  return Return;
 };
 </script>
 
@@ -32,33 +79,31 @@ const CountUR = (ur: string) => {
     <n-space class="data-wrapper">
       <div class="block">
         <span class="label">上涨指数</span>
-        <span class="value">{{ Data.UPIndex }}</span>
+        <span class="value" :class="UPIndex()">{{ Data.UPIndex }}%</span>
       </div>
       <div class="block">
         <span class="label">综合涨幅均值</span>
-        <span class="value">{{ Data.UDAvg }}%</span>
+        <span class="value" :class="UDAvg()">{{ Data.UDAvg }}%</span>
       </div>
       <div class="block">
-        <span class="label">上涨趋势</span>
-        <span class="value">{{ Data.UPLe }}</span>
-      </div>
-      <div class="block">
-        <span class="label">上涨强度</span>
-        <span class="value">{{ Data.UDLe }}</span>
-      </div>
-      <div class="block">
-        <span class="label">当前市场情况</span>
-        <span class="value">{{ Data.DirIndex }}</span>
-      </div>
-
-      <div class="block">
-        <span class="label">最牛币</span>
-        <span class="value">{{ Data.MaxUP.CcyName }} {{ Data.MaxUP.U_R24 }} </span>
+        <span class="label">市场整体情况</span>
+        <span class="value" :class="DirIndex().style">{{ DirIndex().text }}</span>
       </div>
 
       <div class="block">
         <span class="label">最惨币</span>
-        <span class="value">{{ Data.MaxDown.CcyName }} {{ Data.MaxDown.U_R24 }} </span>
+        <span class="value" :class="CoinTicker('MaxDown').style">
+          {{ CoinTicker('MaxDown').CcyName }}
+          {{ CoinTicker('MaxDown').U_R24 }}%
+        </span>
+      </div>
+
+      <div class="block">
+        <span class="label">最牛币</span>
+        <span class="value" :class="CoinTicker('MaxUP').style">
+          {{ CoinTicker('MaxUP').CcyName }}
+          {{ CoinTicker('MaxUP').U_R24 }}%
+        </span>
       </div>
 
       <div class="block">
@@ -66,6 +111,7 @@ const CountUR = (ur: string) => {
         <span class="value"><n-time :time="Data.Ts" /></span>
       </div>
     </n-space>
+    <div>持仓建议：{{ Data.Suggest }}</div>
   </div>
 </template>
 
