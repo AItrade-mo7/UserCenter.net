f13501f6e197ff1a24be8daa7d17e21b4adbdc48
墨七 <meichangliang@mo7.cc>
2022-09-30 02:16:20 +0800

    exit-push

--- _shell/build.sh
+++ _shell/build.sh
@@ -3,31 +3,16 @@
 source "./_shell/init.sh"
 #############
 
-echo " =========== 整理文件到 www  =========== "
-rm -rf ${staticPath}
-mv ${outPutPath} ${staticPath}
+echo "开始打包" &&
+  npm run build
 
-echo " =========== 写入文件 =========== "
-sudo cat >${staticPath}"/Index.go" <<END
-package www
+echo "停止 pm2 服务" &&
+  pm2 delete ${startName}
 
-import "embed"
+echo "移动文件到 ProdProject 目录"
+cp -r ${outPutPath}"/." ${deployPath}"/"
 
-//go:embed * assets/*
-var Static embed.FS
+cd ${deployPath}
 
-END
-
-echo " =========== go build  =========== "
-
-go mod tidy &&
-  go build -o ${buildName}
-echo " server 端编译 完成"
-
-echo " =========== 开始进行文件整合 =========== "
-
-mkdir ${outPutPath}
-
-echo "移动 go build 文件"
-mv ${buildName} ${outPutPath}"/" &&
-  exit
+echo "启动 pm2 服务"
+pm2 start ./${buildName} --name ${startName}
