abbb4c466a5bea8b58a2b13610e8520e54dc8ec2
墨七 <meichangliang@mo7.cc>
2022-10-14 02:42:52 +0800

    exit-push

--- server/router/middle/Public.go
+++ server/router/middle/Public.go
@@ -10,17 +10,17 @@ func Public(c *fiber.Ctx) error {
 	// 添加访问头
 	AddHeader(c)
 
-	return c.Next()
-}
-
-func AddHeader(c *fiber.Ctx) error {
-	c.Set("Data-Path", "DataCenter.net")
-
 	// 授权验证
 	err := EncryptAuth(c)
 	if err != nil {
 		return c.JSON(result.ErrAuth.WithData(mStr.ToStr(err)))
 	}
+
+	return c.Next()
+}
+
+func AddHeader(c *fiber.Ctx) error {
+	c.Set("Data-Path", "DataCenter.net")
 
 	return nil
 }
