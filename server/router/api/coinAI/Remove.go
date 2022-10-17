232cebb5923ca6e0d6a78fbac944564ed21d1dce
墨七 <meichangliang@mo7.cc>
2022-10-17 15:05:44 +0800

    exit-push

--- server/router/api/coinAI/Remove.go
+++ server/router/api/coinAI/Remove.go
@@ -1,10 +1,63 @@
 package coinAI
 
 import (
+	"DataCenter.net/server/global/config"
+	"DataCenter.net/server/global/dbType"
+	"DataCenter.net/server/router/middle"
 	"DataCenter.net/server/router/result"
+	"github.com/EasyGolang/goTools/mFiber"
+	"github.com/EasyGolang/goTools/mMongo"
+	"github.com/EasyGolang/goTools/mStr"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type RemoveCoinAIParam struct {
+	ServeID string
+}
+
 func Remove(c *fiber.Ctx) error {
-	return c.JSON(result.Succeed.WithMsg("删除接口尚在开发中"))
+	var json RemoveCoinAIParam
+	mFiber.Parser(c, &json)
+
+	if len(json.ServeID) < 3 {
+		return c.JSON(result.Fail.WithMsg("缺少 ServeID"))
+	}
+
+	userID, err := middle.TokenAuth(c)
+	if err != nil {
+		return c.JSON(result.ErrToken.WithData(mStr.ToStr(err)))
+	}
+
+	db := mMongo.New(mMongo.Opt{
+		UserName: config.SysEnv.MongoUserName,
+		Password: config.SysEnv.MongoPassword,
+		Address:  config.SysEnv.MongoAddress,
+		DBName:   "AITrade",
+	}).Connect().Collection("CoinAINet")
+	defer db.Close()
+
+	findOpt := options.FindOne()
+	findOpt.SetSort(map[string]int{
+		"TimeUnix": -1,
+	})
+
+	findFK := bson.D{{
+		Key:   "ServeID",
+		Value: json.ServeID,
+	}}
+	var CoinServe dbType.AppEnv
+	db.Table.FindOne(db.Ctx, findFK, findOpt).Decode(&CoinServe)
+	if len(CoinServe.ServeID) < 3 {
+		return c.JSON(result.Fail.WithMsg("未找到该服务"))
+	}
+	if CoinServe.UserID != userID {
+		return c.JSON(result.Fail.WithMsg("该 CoinAI 不属于当前用户"))
+	}
+
+	// 在这里 ping 一下
+
+	db.Table.DeleteOne(db.Ctx, findFK)
+	return c.JSON(result.Succeed.WithMsg("已删除"))
 }
