07ae73583b83d5b8e732a8b42c70761a3002400a
墨七 <mo7@mo7.cc>
2023-02-13 03:53:38 +0800

    exit-push

--- server/global/task/SendEmail.go
+++ server/global/task/SendEmail.go
@@ -9,27 +9,52 @@ import (
 
 func TestSendMsg() {
 	Cont := mTask.ToMapData(mTask.SendEmail{
-		From: "AITrade",
+		From: "AITrade.net",
 		To: []string{
 			"meichangliang@outlook.com",
+			"mo7@mo7.cc",
 		},
-		Subject:  "Subjectxxx",
+		Subject:  "测试邮件!",
 		TmplName: "SysEmail",
 		SendData: mTask.SysEmailParam{
-			Title:        "系统提示",
-			Message:      "启动系统:",
+			Title:        "测试邮件",
+			Message:      "测试一下邮件的发送",
 			Content:      "这是一封来自系统的测试邮件",
 			SysTime:      mTime.UnixFormat(mTime.GetUnixInt64()),
 			Source:       Source,
 			SecurityCode: "trade.mo7.cc",
 		},
+
+		// TmplName: "CodeEmail",
+		// SendData: mTask.CodeEmailParam{
+		// 	VerifyCode:   "045685",
+		// 	Action:       "测试",
+		// 	SysTime:      mTime.UnixFormat(mTime.GetUnixInt64()),
+		// 	Source:       Source,
+		// 	SecurityCode: "trade.mo7.cc",
+		// },
+
+		// TmplName: "RegisterEmail",
+		// SendData: mTask.RegisterParam{
+		// 	Password:     "SDGASDas",
+		// 	SysTime:      mTime.UnixFormat(mTime.GetUnixInt64()),
+		// 	Source:       Source,
+		// 	SecurityCode: "trade.mo7.cc",
+		// },
 	})
 
-	err := NewTask(NewTaskOpt{
-		TaskType:    "SendEmail",
-		Content:     Cont,
-		Description: "测试一波发送",
-	})
+	start := mTime.GetUnixInt64()
+	fmt.Println("开始创建 5 个任务", start)
+	for i := 0; i < 5; i++ {
+		err := NewTask(NewTaskOpt{
+			TaskType:    "SendEmail",
+			Content:     Cont,
+			Description: "测试一波发送",
+		})
+		fmt.Println(i, err)
+	}
 
-	fmt.Println(err)
+	end := mTime.GetUnixInt64()
+	fmt.Println("end", end)
+	fmt.Println("用时", end-start)
 }
