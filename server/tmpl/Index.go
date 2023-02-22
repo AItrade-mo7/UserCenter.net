6024ae7d385130e375c561a15b1903cd74331a53
墨七 <mo7@mo7.cc>
2023-02-22 10:04:49 +0800

    清理多余文件

--- server/tmpl/Index.go
+++ server/tmpl/Index.go
@@ -4,34 +4,6 @@ import (
 	_ "embed"
 )
 
-//go:embed email-sys.html
-var SysEmail string
-
-type SysParam struct {
-	Message      string
-	SysTime      string
-	SecurityCode string
-}
-
-//go:embed email-code.html
-var CodeEmail string
-
-type CodeParam struct {
-	VerifyCode   string
-	Action       string
-	SysTime      string
-	SecurityCode string
-}
-
-//go:embed email-register.html
-var RegisterSucceedEmail string
-
-type RegisterSucceedParam struct {
-	Password     string
-	SysTime      string
-	SecurityCode string
-}
-
 //go:embed inst_CoinAI.sh
 var InstCoinServe string
 
@@ -39,9 +11,6 @@ type InstCoinServeParam struct {
 	Port   string
 	UserID string
 }
-
-//go:embed SignIn.py
-var SignInPy string
 
 //go:embed FreedomShell.sh
 var FreedomShell string
