6f0f0116377dad0907890684e4291d099e5a019e
墨七 <mo7@mo7.cc>
2023-03-17 01:15:24 +0800

    修改结构

--- server/global/dbType/CoinAI.go
+++ server/global/dbType/CoinAI.go
@@ -6,22 +6,14 @@ db: AIServe
 collection : CoinAI
 */
 
-type OkxKeyType struct {
-	Name       string `bson:"Name"`
-	ApiKey     string `bson:"ApiKey"`
-	SecretKey  string `bson:"SecretKey"`
-	Passphrase string `bson:"Passphrase"`
-	UserID     string `bson:"UserID"` // 用户 ID 必填项 ，禁止野生账户的存在
-}
-
 type AppEnvType struct {
-	SysName      string       `bson:"SysName"`      // 系统的名字  ， 自动生成项
-	SysVersion   string       `bson:"SysVersion"`   // 系统的版本  ， 自动回填
-	UserID       string       `bson:"UserID"`       // 用户名字 必填项  ， 禁止野生主机的存在
-	Port         string       `bson:"Port"`         // 系统运行的端口 , 用户必填项
-	IP           string       `bson:"IP"`           // 系统运行的 IP, 为自动获取回填
-	ServeID      string       `bson:"ServeID"`      // ServeID ，  ip+端口
-	MaxApiKeyNum int          `bson:"MaxApiKeyNum"` // 最大 Api 数量限制
-	Type         string       `bson:"Type"`
-	ApiKeyList   []OkxKeyType `bson:"ApiKeyList"` // ApiKey 列表
+	SysName      string `bson:"SysName"`      // 系统的名字  ， 自动生成项
+	SysVersion   string `bson:"SysVersion"`   // 系统的版本  ， 自动回填
+	UserID       string `bson:"UserID"`       // 用户名字 必填项  ， 禁止野生主机的存在
+	Port         string `bson:"Port"`         // 系统运行的端口 , 用户必填项
+	IP           string `bson:"IP"`           // 系统运行的 IP, 为自动获取回填
+	ServeID      string `bson:"ServeID"`      // ServeID ，  ip+端口
+	MaxApiKeyNum int    `bson:"MaxApiKeyNum"` // 最大 Api 数量限制
+	Type         string `bson:"Type"`
+	ApiKeyList   []any  `bson:"ApiKeyList"` // ApiKey 列表
 }
