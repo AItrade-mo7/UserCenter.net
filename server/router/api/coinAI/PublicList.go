9b80e8e67a4810e91e0f403cba5916121e3fda4f
墨七 <mo7@mo7.cc>
2023-03-26 19:42:04 +0800

    公开的卫星服务列表

--- server/router/api/coinAI/PublicList.go
+++ server/router/api/coinAI/PublicList.go
@@ -1,10 +1,14 @@
 package coinAI
 
 import (
+	"UserCenter.net/server/global/config"
 	"UserCenter.net/server/global/dbType"
 	"UserCenter.net/server/global/middle"
 	"UserCenter.net/server/router/result"
+	"github.com/EasyGolang/goTools/mMongo"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func PublicList(c *fiber.Ctx) error {
@@ -13,44 +17,34 @@ func PublicList(c *fiber.Ctx) error {
 		return c.JSON(result.Fail.With("获取失败", "设备异常"))
 	}
 
-	// UserDB, err := dbUser.NewUserDB(dbUser.NewUserOpt{
-	// 	Email: "config.SysEmail",
-	// })
-	// if err != nil {
-	// 	return c.JSON(result.ErrDB.WithData(err))
-	// }
+	db := mMongo.New(mMongo.Opt{
+		UserName: config.SysEnv.MongoUserName,
+		Password: config.SysEnv.MongoPassword,
+		Address:  config.SysEnv.MongoAddress,
+		DBName:   "AIServe",
+	}).Connect().Collection("CoinAI")
+	defer db.Close()
 
-	// db := mMongo.New(mMongo.Opt{
-	// 	UserName: config.SysEnv.MongoUserName,
-	// 	Password: config.SysEnv.MongoPassword,
-	// 	Address:  config.SysEnv.MongoAddress,
-	// 	DBName:   "AItrade",
-	// }).Connect().Collection("CoinAINet")
-	// defer db.Close()
+	findOpt := options.Find()
+	findOpt.SetAllowDiskUse(true)
+	findOpt.SetSort(map[string]int{
+		"TimeUnix": 1,
+	})
 
-	// findOpt := options.Find()
-	// findOpt.SetAllowDiskUse(true)
-	// findOpt.SetSort(map[string]int{
-	// 	"TimeUnix": -1,
-	// })
+	findFK := bson.D{{
+		Key:   "IsPublic",
+		Value: true,
+	}}
+	cursor, err := db.Table.Find(db.Ctx, findFK, findOpt)
+	if err != nil {
+		return c.JSON(result.ErrDB.WithData(err))
+	}
 
-	// findFK := bson.D{{
-	// 	Key:   "UserID",
-	// 	Value: UserDB.AccountData.UserID,
-	// }}
-
-	// cursor, err := db.Table.Find(db.Ctx, findFK, findOpt)
-	// if err != nil {
-	// 	return c.JSON(result.ErrDB.WithData(err))
-	// }
-
-	// CoinAIList := []dbType.AppEnv{}
-	// for cursor.Next(db.Ctx) {
-	// 	var CoinServe dbType.AppEnv
-	// 	cursor.Decode(&CoinServe)
-	// 	CoinAIList = append(CoinAIList, CoinServe)
-	// }
-
-	// return c.JSON(result.Succeed.WithData(CoinAIList))
-	return c.JSON(result.Succeed.WithData([]dbType.AppEnvType{}))
+	CoinAIList := []dbType.AppEnvType{}
+	for cursor.Next(db.Ctx) {
+		var CoinServe dbType.AppEnvType
+		cursor.Decode(&CoinServe)
+		CoinAIList = append(CoinAIList, CoinServe)
+	}
+	return c.JSON(result.Succeed.WithData(CoinAIList))
 }
