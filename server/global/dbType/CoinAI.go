d53a066ff273fede17d729aba667ae9f921b0d4d
墨七 <mo7@mo7.cc>
2023-04-10 10:46:45 +0800

    新增Type

--- server/global/dbType/CoinAI.go
+++ server/global/dbType/CoinAI.go
@@ -8,6 +8,7 @@ collection : CoinAI
 
 type AppEnvType struct {
 	SysName      string `bson:"SysName"`      // 系统的名字  ， 自动生成项
+	SysType      string `bson:"SysType"`      // 系统的类型
 	Describe     string `bson:"Describe"`     // 描述
 	SysVersion   string `bson:"SysVersion"`   // 系统的版本  ， 自动回填
 	UserID       string `bson:"UserID"`       // 用户名字 必填项  ， 禁止野生主机的存在
@@ -18,5 +19,5 @@ type AppEnvType struct {
 	IsPublic     bool   `bson:"IsPublic"`     // 是否公开此服务
 	CreateTime   int64  `bson:"CreateTime"`   // 创建时间
 	UpdateTime   int64  `bson:"UpdateTime"`   // 更新时间
-	ApiKeyList   []any  `bson:"ApiKeyList"`   // ApiKey 列表
+	ApiKeyList   any    `bson:"ApiKeyList"`   // ApiKey 列表
 }
